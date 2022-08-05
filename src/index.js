b3938feb544c0af0356ddac3a5736ab0a7a17224
LuijoO <luijohm@gmail.com>
2022-08-05 13:23:18 +0200

    revisar el index.js. han habido cambios.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from "./App"
+import { QueryClient, QueryClientProvider } from "react-query";
 
-// ReactDOM.render(
-//   <div><h1>Template</h1><div><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A maiores ullam, praesentium explicabo aliquam dolores incidunt repudiandae adipisci, repellat, neque quae veritatis corrupti! Magnam, alias iusto expedita minima laudantium nihil.</p></div></div>,
-//   document.getElementById('root')
-// );
+const queryClient = QueryClient();
 
 ReactDOM.render (
-  <App />,
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
   document.getElementById("root")
 );
 
